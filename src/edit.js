0f0a6f684b6f25ace0b59a9315e18fd3c397157d
born2snipe <born2snipe@gmail.com>
2012-03-21 19:34:55 -0500

    introduced a toolbar and got rid of the original buttons

--- src/edit.js
+++ src/edit.js
@@ -1,9 +1,13 @@
 var editorId = "ace_editor";
 var editor;
+var editorView;
+var editorToolbar;
 
 $(function() {
   hideTheDefaultEditorWindow();
+  insertEditorView();
   insertTheNewEditorWindow();
+  insertToolbar();
   updateTheSave();
 });
 
@@ -14,12 +18,42 @@ function updateTheSave() {
   });
 }
 
+function insertEditorView() {
+  editorView = document.createElement("div");
+  editorView.setAttribute("id", "editor_view");
+  editorView.setAttribute("style", "width: 640px; height: 480px; margin: 110px auto auto 140px;");
+  $("body").append(editorView);
+}
+
+function insertToolbar() {
+  editorToolbar = document.createElement("div");
+  editorToolbar.setAttribute("id", "editor_toolbar");
+  editorToolbar.setAttribute("style", "background-color:#DDDDDD;border-top:1px solid #999999;border-bottom:1px solid #999999;padding:5px 10px 5px 10px;");
+  $(editorView).prepend(editorToolbar);
+
+  toolbarButton("Save");
+  toolbarButton("Format");
+}
+
+function toolbarButton(text) {
+  var button = document.createElement("a");
+  button.setAttribute("id", "toolbar_"+text);
+  button.innerHTML = text;
+  button.setAttribute("style", "color: #666666;padding:2px;background-color:#DDDDDD;border-top:1px solid #BBBBBB;border-left:1px solid #BBBBBB;border-right:1px solid #999999;border-bottom:1px solid #999999;margin-right:10px;font-family:verdana;font-size:10pt;cursor:pointer;");
+  $(editorToolbar).append(button);
+  $(button).hover(function() {
+    $(this).css("color", "#000000").css("background-color", "#EEEEEE");
+  }, function() {
+    $(this).css("color", "#666666").css("background-color", "#DDDDDD");
+  });
+}
+
 function insertTheNewEditorWindow() {
   var editorElement = document.createElement("div");
   editorElement.setAttribute("id", editorId);
   editorElement.innerHTML = $(pageContentId).html();
-  editorElement.setAttribute("style", "width: 640px; height: 480px; margin: 110px auto auto 140px;");
-  $("body").prepend(editorElement);
+  editorElement.setAttribute("style", "width: 640px; height: 480px;");
+  $(editorView).append(editorElement);
  
   editor = ace.edit(editorId);
 
@@ -30,7 +64,10 @@ function insertTheNewEditorWindow() {
 }
 
 function hideTheDefaultEditorWindow() {
-  $("textarea").css("visibility", "hidden");
+  $("textarea").css("display", "none");
+  $(".edit_buttons").css("display", "none");
+  $(".edit_options").css("display", "none");
+  $(".hints").css("display", "none");
 }
 
 function loadKeybinding(editor) {
